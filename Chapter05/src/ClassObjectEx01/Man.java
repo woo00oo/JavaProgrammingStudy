b6c2b5414a2e3312a329bd6ac6d816a8d90da31e
as2396 <57715611+as2396@users.noreply.github.com>
2020-07-09 00:13:56 +0900

    Chapter05 Class

---
+++ Chapter05/src/ClassObjectEx01/Man.java
@@ -0,0 +1,17 @@
+package ClassObjectEx01;
+
+public class Man {
+	
+	int age;
+	String name;
+	boolean wedding;
+	int baby;
+	
+	public void showInfo() {
+		System.out.println("나이 : " + age );
+		System.out.println("이름 : " + name );
+		System.out.println("결혼여부 : " + wedding );
+		System.out.println("자녀 수 : " + baby );
+	}
+
+}
---
+++ Chapter05/src/ClassObjectEx01/manTest.java
@@ -0,0 +1,17 @@
+package ClassObjectEx01;
+
+public class manTest {
+
+	public static void main(String[] args) {
+		
+		Man man = new Man();
+		man.age = 40;
+		man.name = "James";
+		man.wedding = true;
+		man.baby = 3;
+		
+		man.showInfo();
+
+	}
+
+}
