c0f77349da8fa6e981a0e8a2781e0dfe46f5358d
as2396 <57715611+as2396@users.noreply.github.com>
2020-07-09 00:41:00 +0900

    Ex03

--- Chapter05/src/ClassObjectEx02/Order.java
+++ Chapter05/src/ClassObjectEx02/Order.java
@@ -1,5 +1,12 @@
 package ClassObjectEx02;
 
 public class Order {
+	
+	long orderId;
+	String customerId;
+	String orderDate;  
+	String customerName;
+	String productId;
+	String shippingAddress;
 
 }
