0f3e58dad7716d0c8cb13828551caca8c215958c
as2396 <57715611+as2396@users.noreply.github.com>
2020-07-27 00:00:52 +0900

    ExceptionEx02

---
+++ Chapter13/src/exception/AutoCloseTest.java
@@ -0,0 +1,15 @@
+package exception;
+
+public class AutoCloseTest {
+
+	public static void main(String[] args) {
+
+		
+		try( AutoCloseObj obj = new AutoCloseObj()){
+			
+		}catch(Exception e) {
+			
+		}
+	}
+
+}
